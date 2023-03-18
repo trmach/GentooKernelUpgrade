7289815876628cddf0176caadbd605bc55cc6d31
trmach <charlessaineghi@gmail.com>
2023-03-18 11:09:41 -0500

    :radio_button:

--- gku.c
+++ gku.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <gtk-3.0/gtk/gtk.h>
 
 #define ENTRY_BUFFER 100
@@ -39,16 +40,21 @@ static void update_button(GtkWidget *button, gpointer data) {
 
 static int activate(GtkApplication* app, gpointer user_data) {
 
-  GtkWidget *window, *number_button, *cancel_button, *button_box;
+  GtkWidget *window, 
+    *box,
+    *number_button, *cancel_button, 
+    *radio1, *radio2;
   
   //* Open Window and set properties
-  window = gtk_application_window_new (app);
+  window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW (window), "bitchass");
   gtk_window_set_default_size(GTK_WINDOW (window), 640, 480);
   
-  //* Thing that contains buttons
-  button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
-  gtk_container_add(GTK_CONTAINER (window), button_box);
+  //* Make a box, not a button_box!
+  box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 10);
+  gtk_box_set_homogeneous(GTK_BOX (box), TRUE);
+
+  gtk_container_add(GTK_CONTAINER (window), box);
 
   //* Button that does something fun
   number_button = gtk_button_new_with_label("0");
@@ -58,12 +64,24 @@ static int activate(GtkApplication* app, gpointer user_data) {
   cancel_button = gtk_button_new_with_label("Exit");
   g_signal_connect_swapped(cancel_button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
 
+  //* Radio buttons
+  radio1 = gtk_radio_button_new(NULL);
+  GtkWidget * entry = gtk_entry_new();
+  gtk_container_add(GTK_CONTAINER (radio1), entry);
+
+  radio2 = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON (radio1), "Second button");
+
   //* Add buttons to container. It looks like hell.
-  gtk_container_add(GTK_CONTAINER (button_box), number_button);
-  gtk_container_add(GTK_CONTAINER (button_box), cancel_button);
-  
+  gtk_box_pack_start(GTK_BOX (box), number_button, true, true, 9);
+  gtk_box_pack_start(GTK_BOX (box), cancel_button, true, true, 9);
+  gtk_box_pack_start(GTK_BOX (box), radio1, true, true, 2);
+  gtk_box_pack_start(GTK_BOX (box), radio2, true, true, 2);
+  //gtk_container_add(GTK_CONTAINER (box), radio_button);
+  //gtk_container_add(GTK_CONTAINER (window), box);
 
   gtk_widget_show_all(window);
+
+  return 0;
   
 }
 
